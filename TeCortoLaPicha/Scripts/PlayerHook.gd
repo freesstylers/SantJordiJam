cfebb58f9bd583dc4966247d8360a960e66fa5e2
davidczepiel <dczepiel@ucm.es>
2024-04-07 10:42:16 +0200

    Mejorada grappling rope visualmente

--- TeCortoLaPicha/Scripts/PlayerHook.gd
+++ TeCortoLaPicha/Scripts/PlayerHook.gd
@@ -4,17 +4,23 @@ class_name PlayerHook
 @onready var grappling_rope : GrapplingRope = $RopeStartingPoint/Rope
 @onready var ray : RayCast2D = $RopeStartingPoint/RayCast2D
 @onready var rope_spring : DampedSpringJoint2D = $RopeForceSpring
+@onready var image:Sprite2D = $Sprite2D
 
 var hook_shot = false
 var hook_target : Vector2 = Vector2.ZERO
 
 func _ready():
+	set_can_sleep(false)
 	set_process_input(true) # Enable input processing
 	
 func _process(delta):
 	if not hook_shot:
 		var direction = get_global_mouse_position() - global_position
 		rope_starting_point.global_rotation = direction.angle()
+		image.global_position = get_global_mouse_position()
+	else:
+		rope_starting_point.global_rotation = (hook_target-global_position).angle()
+		image.global_position = hook_target
 		
 
 func _input(event):
@@ -26,8 +32,14 @@ func _input(event):
 				if colliderHit:	
 					hook_shot = true
 					hook_target = ray.get_collision_point()
+					rope_spring.node_a = get_path()
 					rope_spring.node_b= colliderHit.get_path()
-					rope_spring.rest_length = hook_target.distance_to(global_position)/2
+					var distance = hook_target.distance_to(global_position)
+					rope_spring.length = 10#hook_target.distance_to(global_position)/4
+					rope_spring.rest_length = rope_spring.length*0.75#hook_target.distance_to(global_position)/4
+					print(distance,"-",rope_spring.length)
+					get_node(get_path()).set_sleeping(false)
+					#get_node(colliderHit.get_path()).set_sleeping(false)
 					grappling_rope.ShootRope(hook_target)
 					
 		if hook_shot and mouse_event.button_index == MOUSE_BUTTON_LEFT and not mouse_event.pressed:
--- TeCortoLaPicha/Scripts/PlayerRope.gd
+++ TeCortoLaPicha/Scripts/PlayerRope.gd
@@ -8,7 +8,7 @@ class_name GrapplingRope
 @export_range(0.01,4) var WaveHeightMultiplier : float = 2
 @export_range(0,25) var straightenLineSpeed : float = 0.5
 
-@onready var linePath : Path2D = $RopeVisualPath
+@onready var linePath : Path2D = $RopeVisualPath2
 @onready var waveHeight : float = WaveHeightMultiplier
 @onready var m_lineRenderer : Line2D = self
 
@@ -16,6 +16,7 @@ var rope_target : Vector2 = global_position + Vector2(100,0)
 var rope_shot : bool = false
 var straightLine : bool = false;
 var isGrappling : bool = true
+var ropeBakedLength : int = 0
 
 func _ready():
 	LinePointsToFirePoint()
@@ -27,11 +28,13 @@ func _process(delta):
 
 func LinePointsToFirePoint():
 	rope_shot=false
+	ropeBakedLength=0
 	m_lineRenderer.clear_points()
 	for i in range(0,NumRopeSegments+1):
 		m_lineRenderer.add_point(Vector2(0,0))
 		
 func ShootRope(hook_target : Vector2):
+	linePath= get_child(randi()%get_child_count()) as Path2D
 	LinePointsToFirePoint()
 	rope_shot = true
 	straightLine = false;
@@ -45,6 +48,7 @@ func ShootRope(hook_target : Vector2):
 func HideRope():
 	LinePointsToFirePoint()
 	isGrappling = false;
+	linePath=null
 	
 func DrawRope(delta):
 	if (!straightLine):
@@ -72,7 +76,7 @@ func DrawRopeWaves():
 	for vertexIndex in range(0,NumRopeSegments+1):
 		#Get the position of the current vertex inside the curve2D and transform it to global coordinates
 		var curve_local_offset = back_length * (vertexIndex as float/NumRopeSegments as float)
-		var pos_inside_curve = linePath.curve.sample_baked(curve_local_offset) / 100 #X in range [0,100] Y in range [-100,100]
+		var pos_inside_curve = linePath.curve.sample_baked(curve_local_offset) / linePath.curve.get_point_position(linePath.curve.point_count-1).x #X in range [0,100] Y in range [-100,100]
 		var pos_in_world = pos_inside_curve * rope_target.length()
 		#Lerp each point from the firePoint to its end position
 		var originalPos = m_lineRenderer.get_point_position(vertexIndex)
