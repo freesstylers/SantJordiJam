165c4a288a9d3b61894a1db13a033bb39b7b6681
IgnOry <37537092+IgnOry@users.noreply.github.com>
2024-04-23 19:12:12 +0200

    A cerrar godot

--- TeCortoLaPicha/Scripts/BossFight/BossFightManager.gd
+++ TeCortoLaPicha/Scripts/BossFight/BossFightManager.gd
@@ -34,13 +34,14 @@ func start_fight():
 	Dragon.setState("FIREBALL")	
 
 func PlayerWon():
-	if get_tree().root.get_child(1).endless == true:
+	Globals.roomCompleted.disconnect(PlayerWon)
+			
+	if get_tree().root.get_child(1).endless == true:		
 		if get_tree().root.get_child(1).get_child(1).get_child_count() > 0:
 			get_tree().root.get_child(1).get_child(1).get_child(0).get_child(1).get_child(0).visible = false
 		get_tree().root.get_child(1).get_child(0).visible = true
 		
 		get_tree().root.get_node("SceneManager/TransitionScreen").transition()
-		Globals.roomCompleted.disconnect(PlayerWon)
 		#Delete Room
 		self.queue_free()
 		
